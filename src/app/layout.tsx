7d7a894a5e417fdf1f785f2b0da98be648465868
Flemming Madsen <mail@leflings.dk>
2023-09-12 08:19:57 +0200

    decrease padding on smaller screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className='{inter.className} bg-cyan-950 text-cyan-100'>
-        <main className="flex flex-col min-h-screen px-16 h-full">
+        <main className="flex flex-col min-h-screen px-4 lg:px-16 h-full">
           <div className='h-10 w-3 border-l-2 border-b-2 rounded-bl-xl border-yellow-600'></div>
           <div className='bg-gradient-to-r from-yellow-600 to-yellow-300 my-[-2px] rounded-r-xl p-[2px] pl-0 ml-3 self-start'>
             <div className='bg-cyan-950 rounded-r-xl text-6xl font-bold tracking-widest uppercase pl-1 p-4'>
